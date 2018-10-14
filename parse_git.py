a095f08175eb81cf482221f565978454d8ff213c
Elnatan Steinmetz <elnatan@ravtech.co.il>
2018-10-14 17:08:05 +0300

    emil3345

--- parse_git.py
+++ parse_git.py
@@ -1,10 +1,11 @@
 
 import subprocess
-
+import os
 
 f = subprocess.check_output("git --no-pager show -s --format=%ae".split())
 # subprocess.check_output("echo .>out111.txt".split())
 with open("out111.txt", 'w') as r:
     r.write('aa')
 f = str(f.strip().decode())
+os.environ["author_email"] = f
 print(f)
