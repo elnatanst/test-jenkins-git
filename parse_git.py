14826a28eb31c285eedcc7170e77e16a6e0aacdb
Elnatan Steinmetz <elnatan@ravtech.co.il>
2018-10-14 16:31:04 +0300

    emil3345

--- parse_git.py
+++ parse_git.py
@@ -3,7 +3,8 @@ import subprocess
 
 
 f = subprocess.check_output("git --no-pager show -s --format=%ae".split())
-subprocess.check_output("echo.>out111.txt")
-
+# subprocess.check_output("echo .>out111.txt".split())
+with open("out111.txt", 'w') as r:
+    r.write('aa')
 f = str(f.strip())
 print(f)
