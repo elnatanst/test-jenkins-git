63988db4bcf018dcc14622a66f01223d2afa91c2
Elnatan Steinmetz <elnatan@ravtech.co.il>
2018-10-15 15:54:55 +0300

    test12

--- parse_git.py
+++ parse_git.py
@@ -3,5 +3,5 @@ import sys
 # print("get email.....")
 email = subprocess.check_output("git --no-pager show -s --format=%ae".split())
 # email = "aaaa"
-sys.stdout.write(email.decode())
-# print(email.decode())
+# sys.stdout.write(email.decode())
+print(email.decode())
