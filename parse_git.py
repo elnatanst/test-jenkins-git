d7fd28ce3d479af47010472736a26510c62b995c
Elnatan Steinmetz <elnatan@ravtech.co.il>
2018-10-15 13:35:22 +0300

    test1

--- parse_git.py
+++ parse_git.py
@@ -1,4 +1,4 @@
 import subprocess
 
-email = subprocess.check_output("git --no-pager show -s --format=%ae")
-print(str(email.decode()))
+email = subprocess.check_output("git --no-pager show -s --format=%ae".split())
+print(email.decode())
